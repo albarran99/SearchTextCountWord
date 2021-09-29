a0a7bef47692754eff70eb5788c3f9be93cc0e2c
raulalbarran <raulvezon15@gmail.com>
2021-09-29 14:14:22 +0200

    Update

--- src/main/java/SearchTextMain.java
+++ src/main/java/SearchTextMain.java
@@ -1,6 +1,12 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class SearchTextMain {
 
+    private static final Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Introduce un archivo");
+
     }
 }
